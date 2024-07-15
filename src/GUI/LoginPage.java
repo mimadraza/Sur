f08e6535c725b4caa1b7bf5a38bc41185cfb4a65
mimadraza <m.imadraza@gmail.com>
2024-07-15 19:54:15 +0500

    new data structures

---
+++ src/GUI/LoginPage.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginPage extends JPanel implements ActionListener
+{
+    private JButton login;
+    private JButton signup;
+    private TextField userName;
+    private TextField password;
+    private Color backgroundColor;
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
