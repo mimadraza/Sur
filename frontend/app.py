799020319d403d357f7474b89eff873118915445
Saad-Imam <saadlibra10@gmail.com>
2024-11-30 19:53:26 +0500

    Added Session Management for differentiating Artist and User

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template, redirect
 from config import Config
 from flask_cors import CORS
 import gladiator as gl
+#Use session to dictate whether artist is trying to log in or user
+from flask import session
+import os
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
@@ -14,13 +17,18 @@ register_validations = (('first_name', gl.required, gl.type_(str), gl.regex_('^[
                         ('country', gl.required, gl.type_(str), gl.regex_('^[^0-9]*$')), 
                         ('dob', gl.required))
 
+#Need to set a secret key so that no invalid users can access session information
+app.secret_key = os.urandom(24)
+
 @app.route('/')
 def index():  
     return render_template('homepage.html') 
 
 @app.route('/login', methods=["GET","POST"])
 def login():  
-    #Make sure artist and user have different hits on backend
+    #Make sure artist and user have different hits on backend SOLVED: USING SESSION
+    #Get session flag:
+    flag = session.get('flag', False) 
     
     if request.method == "POST":
         data = {"email" : request.form['email'],
@@ -31,7 +39,10 @@ def login():
             # response = request.get_json(backendurl)
             # if (response.statuscode == 200):    
             #     return redirect("userdashboard.html")
-            print('All is Good')
+            if flag: 
+                redirect('/artistdashboard')
+            else:
+                redirect('/userdashboard')
         
     return render_template('LoginPage.html')
 
@@ -62,6 +73,7 @@ def userregister():
 
 @app.route('/artistregister', methods=["GET","POST"])
 def artistregister():
+    
     if request.method == "POST":
         data = {
         "first_name" : request.form["first_name"],
@@ -77,7 +89,7 @@ def artistregister():
             # response = request.get_json(backendurl)
             # if (response.statuscode == 200):    
             #     return redirect("/login")
-            return redirect('/login')
+            return redirect('/set_flag')
     
     return render_template('artistregister.html')
 
@@ -90,6 +102,13 @@ def userdashboard():
 def artistdashboard():
 
     return render_template('artistdashboard.html')
+
+
+@app.route('/set_flag')
+def set_flag():
+    # Flag true means artist is trying to login, else user
+    session['flag'] = True
+    return redirect('/login')
 
 
 if __name__ == '__main__':
