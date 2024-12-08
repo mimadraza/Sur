ccbcf4cab8b0bb6b2a354ce76b73dac57906938e
mimadraza <m.imadraza@gmail.com>
2024-12-09 00:46:16 +0500

    temp

--- backend/server.py
+++ backend/server.py
@@ -3,9 +3,13 @@ from flask_cors import CORS
 from backend.routes.auth_routes import auth_blueprint
 from backend.routes.user_routes import user_blueprint
 
-
 app = Flask(__name__)
 CORS(app)
+
+app.config['MYSQL_HOST'] = '4.145.91.69'
+app.config['MYSQL_USER'] = 'admin1'
+app.config['MYSQL_PASSWORD'] = 'P@ssword123'
+app.config['MYSQL_DB'] = 'SUR'
 
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(user_blueprint, url_prefix='/user')
@@ -17,7 +21,7 @@ def hello_world():
 # def get_music(song_name):
 #     # Assuming the song name is used to get the file path from a database
 #     song_path = 'musics/Song 1.mp3'
-
+#
 #     # Return the file path to the frontend
 #     return send_file(song_path, mimetype='audio/mpeg')
 
