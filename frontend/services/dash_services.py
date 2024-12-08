5fef791f24d76674ba14290a8d204000a95c39dd
Muhammad Hashir Ilyas <muhammadhashir.ilyas@gmail.com>
2024-12-09 04:30:31 +0500

    Home Renovation

--- frontend/services/dash_services.py
+++ frontend/services/dash_services.py
@@ -116,7 +116,8 @@ def album():
 
 def search(string):
      #query database to get one song back
-     response = requests.post(get_backend_url(), json={"search": string})
+     url = f'{get_backend_url()}/user/search'
+     response = requests.post(url, json={"search": string})
      if response.status_code == 200:
         return response.json()
      else:
